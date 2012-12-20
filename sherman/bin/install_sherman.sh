693d38d6b826139efec737a78373bc33a03ad2b7
Barry Robison <barry.robison@gmail.com>
2012-12-19 22:44:58 -0500

    initial install script for centos 6.3

---
+++ sherman/bin/install_sherman.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.i686.rpm
+yum install mercurial
+hg clone https://code.google.com/p/anim-studio-tools/
+yum install rrdtool-devel
+rpm -Uvh rpmforge-release-0.5.2-2.el6.rf.i686.rpm 
+yum install hdf5-devel
+yum install python-genshi
+yum install python-cherrypy
+yum install python-simplejson
+easy_install numpy
+easy_install numexpr
+easy_install Cython
+easy_install tables
+
