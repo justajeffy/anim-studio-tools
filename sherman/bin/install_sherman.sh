872d8b1ad60fa1db06e6c776316a12e808cd93c6
Barry Robison <barry.robison@gmail.com>
2012-12-20 20:35:05 -0500

    more dependencies and custom hdf5 flags accounted for

--- sherman/bin/install_sherman.sh
+++ sherman/bin/install_sherman.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -x
 
+yum install -y wget
 wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.i686.rpm
-yum install mercurial
-hg clone https://code.google.com/p/anim-studio-tools/
-yum install rrdtool-devel
 rpm -Uvh rpmforge-release-0.5.2-2.el6.rf.i686.rpm 
-yum install hdf5-devel
-yum install python-genshi
-yum install python-cherrypy
-yum install python-simplejson
+
+yum install -y mercurial
+hg clone https://code.google.com/p/anim-studio-tools/
+
+yum install -y gcc make gcc-c++
+
+wget http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.10.tar.gz
+tar -zxvf hdf5-1.8.10.tar.gz
+cd hdf5-1.8.10
+./configure --prefix=/usr --enable-threadsafe --with-pthread
+make install
+
+yum install -y rrdtool-devel
+yum install -y python-devel
+yum install -y python-genshi
+yum install -y python-cherrypy
+yum install -y python-simplejson
+yum install -y python-setuptools
+yum install -y libgfortran
 easy_install numpy
 easy_install numexpr
 easy_install Cython
@@ -20,3 +33,4 @@ cd sherman
 mkdir input
 mkdir output
 mkdir rrd
+mkdir log
